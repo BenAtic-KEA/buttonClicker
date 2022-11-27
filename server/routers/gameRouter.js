f374e1ba9a4185ea52f1503351193be869a990b0
Mark Hansen <Mark024e@stud.kea.dk>
2022-11-27 13:12:35 +0100

    only positive saves

--- server/routers/gameRouter.js
+++ server/routers/gameRouter.js
@@ -11,8 +11,12 @@ router.post('/api/saveClicks',async (req, res) => {
     try {
         let username = req.session.user.username
         let clicks = req.body.clicks
-        const result = await saveClicks(username, clicks)
-        res.send({data:{message: "clicks saved"}})
+        if(clicks <= 0){
+            res.status(400).send({data:{message: "has to be a positive number to save"}})
+        } else{
+            const result = await saveClicks(username, clicks)
+            res.send({data:{message: "clicks saved"}})
+        }
     } catch (error) {
         res.status(400).send({data: error})
     }
